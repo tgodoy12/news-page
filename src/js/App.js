f130d5725f48135dd90cd85e5df1cb7df0d7fc51
Trilce Godoy <trilcegodoy11@gmail.com>
2025-01-10 01:08:47 -0300

    navbar added

--- src/js/App.js
+++ src/js/App.js
@@ -3,16 +3,21 @@ import Landing from './pages/landing';
 import Contact from './pages/contact';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './components/navbar';
 
 function App() {
   return (
-   
+   <div className="container px-14">
+
     <BrowserRouter>
+
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/contact" element={<Contact />}/>
       </Routes>
     </BrowserRouter>
+   </div>
 
    
         
--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-
+import Logo from "../../news-homepage-main/assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return(
-        <nav>
+    const navLinks = ["Home", "New", "Popular", "Trending", "Categories"]
 
+    return(
+        <nav className="navbar flex py-6 justify-between">
+            <div className="logo w-11">
+                <img src={Logo} alt="Logo" />
+            </div>
+
+            <div className="flex">
+
+           {navLinks.map((link) => (
+            <div className="nav-links px-5">
+                <Link to="/">{link}</Link>
+            </div>
+           ))}
+            </div>
         </nav>
     )
 }
